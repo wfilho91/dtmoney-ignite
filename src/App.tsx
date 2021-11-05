e6b628994055e077738fe2f5cad795968c370a8d
wfilho91 <wellington79620@gmail.com>
2021-11-05 18:57:47 -0300

    Add component NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
 import { GlobalStyle } from 'styles/global'
 import { Header } from 'components/Header'
 import { Dashboard } from 'components/Dashboard'
-import { createServer } from 'miragejs'
+import Modal from 'react-modal'
+import { useState } from 'react'
+import { NewTransactionModal } from 'components/NewTransactionModal'
 
-createServer({
-  routes() {
-    this.namespace = 'api'
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        }
-      ]
-    })
-  }
-})
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <NewTransactionModal 
+      
+      isOpen={isNewTransactionModalOpen} 
+      onRequestClose ={handleCloseNewTransactionModal}
+
+       />
+
       <GlobalStyle />
     </>
   )
