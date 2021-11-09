4b942db2fb4800089543c41cef702d5ff283a39f
wfilho91 <wellington79620@gmail.com>
2021-11-08 22:21:11 -0300

    Finish component NewTransactionModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,11 @@
 import Modal from 'react-modal'
-import { Container,TransactionTypeContainer} from './styles'
+import { Container, RadioBox, TransactionTypeContainer } from './styles'
 import closeImg from 'assets/close.svg'
 import incomeImg from 'assets/income.svg'
 import outcomeImg from 'assets/outcome.svg'
+import { FormEvent, useState } from 'react'
+import { title } from 'process'
+import { api } from 'services/api'
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -10,37 +13,92 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal(props: NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit')
+  const [title,setTitle] = useState('')
+  const [value,setValue] = useState(0)
+  const [category,setCategory] = useState('')
+
+
+
+  function handleCreateNewTransaction(event : FormEvent){
+     event.preventDefault()
+
+     const data = {
+       title,
+       value,
+       category,
+       type
+     }
+    api.post('/api/transactions',data)
+  }
+
   return (
-    <Modal 
-    isOpen={props.isOpen} 
-    onRequestClose={props.onRequestClose}
-    overlayClassName="react-modal-overlay"
-    className="react-modal-content"
+    <Modal
+      isOpen={props.isOpen}
+      onRequestClose={props.onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <button type="button" 
-      onClick={props.onRequestClose}
-      className="react-modal-close"
+      <button
+        type="button"
+        onClick={props.onRequestClose}
+        className="react-modal-close"
       >
-          <img src={closeImg} alt="Fechar modal" />
+        <img src={closeImg} alt="Fechar modal" />
       </button>
-      <Container>
+
+      <Container onSubmit={handleCreateNewTransaction}>
+
         <h2>Cadastrar transação</h2>
-        <input type="text"  placeholder="Título"/>
-        <input type="number" placeholder="Valor" />
+
+        <input 
+        type="text" 
+        placeholder="Título" 
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        />
+
+        <input 
+        type="number" 
+        placeholder="Valor" 
+        value = {value}
+        onChange = {(event) => setValue(Number(event.target.value))}
+        />
+
         <TransactionTypeContainer>
-            <button type="button">
-                <img src={incomeImg} alt="Entrada" />
-                <span>Entrada</span>
-            </button>
-            <button type="button">
-                <img src={outcomeImg} alt="Saída" />
-                <span>Saída</span>
-            </button>
+
+          <RadioBox 
+          type="button" 
+          onClick={() => setType('deposit')}
+          isActive = {type === 'deposit'}
+          activeColor = 'green'
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox 
+          type="button" 
+          onClick={() => setType('withdraw')}
+          isActive = {type === 'withdraw'}
+          activeColor = 'red'
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+
         </TransactionTypeContainer>
-        <input type="text" placeholder="Categoria" />
+
+        <input 
+        type="text" 
+        placeholder="Categoria" 
+        value = {category}
+        onChange = {(event) => setCategory(event.target.value)}
+        />
         <button type="submit">Cadastrar</button>
+
       </Container>
-      
     </Modal>
   )
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
   h2 {
@@ -51,33 +51,46 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 0.5rem;
+`
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
+interface RadioBoxProps {
+  isActive: boolean
+  activeColor: 'green' | 'red'
+}
 
-    background: transparent;
+const colors = {
+  green: '#33CC95',
+  red: '#E52e54'
+}
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: border-color 0.2s;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')};
-    }
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : 'transparent'};
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `
